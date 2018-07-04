8b40519a9e055896eedfaa586c56059aa4f78dd6
NABIL-PC\Asus <fikri.winaji@gmail.com>
2018-07-04 14:08:38 +0700

    Crud murid
    edit masih error

---
+++ application/controllers/Murid.php
@@ -0,0 +1,88 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Murid extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->model('MuridModel');
+		$data['murid_list'] = $this->MuridModel->getData();
+		$this->load->view('murid_view', $data);
+	}
+	public function insert()
+	{
+		$this->load->library('form_validation');
+		$this->load->model('MuridModel');
+		$this->form_validation->set_rules('nama_murid', 'Nama Lengkap', 'trim|required');
+		$this->form_validation->set_rules('alamat', 'Alamat', 'trim|required');
+		$this->form_validation->set_rules('tanggal', 'Tangal Lahir', 'trim|required');
+		$this->form_validation->set_rules('telp', 'No Handphone', 'trim|required');
+		$this->form_validation->set_rules('jenisKelamin', 'Jenis Kelamin', 'trim|required');
+		$this->form_validation->set_rules('jenjang', 'Jenjang', 'trim|required');
+		$this->form_validation->set_rules('username', 'Username', 'trim|required');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+	
+		$this->form_validation->set_error_delimiters('<div class="text-danger">', '</div>');
+
+		if($this->form_validation->run()==FALSE){
+			$this->load->view('tambahMurid_view.php'); 
+		}
+		else{
+			$upload = $this->MuridModel->upload();
+			if($upload['result'] == "success"){ 
+				$this->MuridModel->insertData($upload['file']['file_name']);
+				redirect('Murid');
+			}else{
+				$data['message'] = $upload['error'];
+				$this->load->view('tambahMurid_view.php',$data); 
+			}
+		}
+	}
+	public function update($id)
+	{
+		$this->load->library('form_validation');
+		$this->load->model('MuridModel');
+		$this->form_validation->set_rules('nama_murid', 'Nama Lengkap', 'trim|required');
+		$this->form_validation->set_rules('alamat', 'Alamat', 'trim|required');
+		$this->form_validation->set_rules('tanggal', 'Tangal Lahir', 'trim|required');
+		$this->form_validation->set_rules('telp', 'No Handphone', 'trim|required');
+		$this->form_validation->set_rules('jenisKelamin', 'Jenis Kelamin', 'trim|required');
+		$this->form_validation->set_rules('jenjang', 'Jenjang', 'trim|required');
+		$this->form_validation->set_rules('username', 'Username', 'trim|required');
+		$this->form_validation->set_rules('password', 'Password', 'trim|required');
+	
+		$this->form_validation->set_error_delimiters('<div class="text-danger">', '</div>');
+		$data['getData'] = $this->MuridModel->getDataWhereId($id)[0];
+
+		if($this->form_validation->run()==FALSE){
+			$this->load->view('updateMurid_view',$data);
+		}
+		else{
+			if ($_FILES['foto']['name'] == "")
+			{
+				$this->MuridModel->updateData($id);
+				redirect('Murid');
+			}
+			else
+			{
+				$upload = $this->MuridModel->upload();
+				if($upload['result'] == "success"){ 
+					$this->MuridModel->updateData($id,$upload['file']['file_name']);
+					redirect('Murid');
+				}else{ 
+					$data['error_upload'] = $upload['error'];
+					$this->load->view('updateMurid_view',$data);
+				}
+			}
+		}
+	}
+
+	public function delete($id)
+	{
+		$this->load->model('MuridModel');
+		$this->MuridModel->hapusdata($id);
+		redirect('Murid');
+	}
+}
+/* End of file murid.php */
+/* Location: ./application/controllers/murid.php */
---
+++ application/models/MuridModel.php
@@ -0,0 +1,86 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MuridModel extends CI_Model {
+
+	public function getData()
+	{
+		$this->db->select('*');
+		$this->db->from("murid");
+		
+		$query = $this->db->get();
+		return $query->result_array();
+	}
+	
+	public function getDataWhereId($id)
+	{
+		$this->db->select('*');
+		$this->db->from("murid");
+		$this->db->where('id',$id);
+		return $this->db->get()->result_array();
+	}
+
+	public function insertData($upload_name)
+	{
+		$data = $this->input->post();
+		unset($data['username']);
+		unset($data['password']);
+
+		$data['foto'] = $upload_name;
+		$this->db->insert("murid",$data);
+		$id_murid = $this->db->insert_id();
+		$set_user = array(
+			'username' => $this->input->post('username'),
+			'password' => md5($this->input->post('password')),
+			'fk_id_murid' => $id_murid
+		);
+		$this->db->insert('user',$set_user);
+
+	}
+
+	public function updateData($id,$upload_name=null)	
+	{
+		$data = $this->input->post();
+		
+		if($upload_name!=null){
+			$data['foto'] = $upload_name;
+			$this->delete_image($id);
+		}
+		$this->db->where('id',$id);
+		if($this->db->update("murid",$data)){
+			return "Berhasil";
+		}
+	}
+
+	public function hapusData($id)
+	{
+		$this->delete_image($id);
+		$this->db->where('id',$id);
+		if($this->db->delete("murid")){
+			return "Berhasil";
+		}
+	}
+
+	public function upload(){
+        $config['upload_path'] = './assets/img/murid/';
+        $config['allowed_types'] = 'jpg|png|jpeg';
+        $config['max_size'] = '2048';
+        $config['remove_space'] = TRUE;
+        $this->load->library('upload', $config);
+        if($this->upload->do_upload('foto')){
+            $return = array('result' => 'success', 'file' => $this->upload->data(),
+            'error' => '');
+            return $return;
+        }else{
+            $return = array('result' => 'failed', 'file' => '', 'error' =>
+            $this->upload->display_errors());
+            return $return;
+        }
+    }
+
+    public function delete_image($id)
+    {
+    	$data = $this->db->get_where('murid',array('id'=>$id))->result_array();
+		unlink('./assets/img/murid/'.$data[0]['foto']);
+    }
+}
